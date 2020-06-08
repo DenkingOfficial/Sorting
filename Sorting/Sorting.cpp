92af584b876ff46e035cbeb4d20aaefd0a7d451e
Denking <shershnev.andrei@gmail.com>
2020-06-08 23:30:18 +0500

    Implemented Search and Random Filling

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
+#include <random>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int Search(int* array, int size, int searchFor)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		if (array[i] == searchFor)
+			return array[i];
+	}
+	return 0;
+}
+
+void FillArrayRnd(int* array, int size, int min, int max)
+{
+	default_random_engine generator(time(0));
+	uniform_int_distribution<int> random(min, max);
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = random(generator);
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int searchCheck[1000];
+	FillArrayRnd(searchCheck, 1000, -500, 500);
+	cout << "Linear search result: " << Search(searchCheck, 1000, 228) << endl;
 }
